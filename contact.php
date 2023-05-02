4dfae79c168eb4910e1e6e84c5d3d10dc0e3dc98
Dee Manning <donald.manning@plus3it.com>
2023-05-02 07:51:54 -0400

    Updated php file

--- contact.php
+++ contact.php
@@ -1,9 +1,9 @@
 <?php
-if (isset($_POST[donald.manning@plus3it.com]) && !empty($_POST[donald.manning@plus3it.com])) {
+if (isset($_POST['donald.manning@plus3it.com']) && !empty($_POST['donald.manning@plus3it.com'])) {
   $subject = "New User request: $_POST[New User request]";
   $message = $_POST[The following request is required for the following user.];
   $headers = From: info@website.com . "\r\n" .
-             'Reply-To: ' . $_POST[donald.manning@plus3it.com]. "\r\n" .
+             'Reply-To: ' . $_POST['donald.manning@plus3it.com']. "\r\n" .
              'X-Mailer: PHP/' . phpversion();
 
   mail(info@example.com, $subject, $message, $headers);
