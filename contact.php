e854c4e9e666f23a29676c33be736e95f41a98cc
Dee Manning <donald.manning@plus3it.com>
2023-05-02 07:55:42 -0400

    Updated string

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_POST['donald.manning@plus3it.com']) && !empty($_POST['donald.manning@plus3it.com'])) {
-  $subject = "New User request: $_POST[New User request]";
+  $subject = "New User request: $_POST['New User request']";
   $message = $_POST[The following request is required for the following user.];
   $headers = From: info@website.com . "\r\n" .
              'Reply-To: ' . $_POST['donald.manning@plus3it.com']. "\r\n" .
