efe8c42204a8283d0f56e7575a3f3dd882074fa7
Dee Manning <donald.manning@plus3it.com>
2023-05-02 09:05:53 -0400

    Removed headers

--- contact.php
+++ contact.php
@@ -7,12 +7,7 @@
          $message .= $_POST['keycloakusername'];
          $message .= $_POST['adminusers'];
          
-         $header = "From:abc@somedomain.com \r\n";
-         $header .= "Cc:afgh@somedomain.com \r\n";
-         $header .= "MIME-Version: 1.0\r\n";
-         $header .= "Content-type: text/html\r\n";
-         
-         $retval = mail ($to,$subject,$message,$header);
+         $retval = mail ($to,$subject,$message);
          
          if( $retval == true ) {
             echo "Message sent successfully...";
