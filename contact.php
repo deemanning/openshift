ad0adf20cbd91c9803ca69d4d1b0bcb9e8010da0
Dee Manning <donald.manning@plus3it.com>
2023-05-03 08:12:24 -0400

    Updated contact php file

--- contact.php
+++ contact.php
@@ -1,21 +1,42 @@
 <?php
-         $to = "donald.manning@plus3it.com";
-         $subject = "New User Request";
-         
-         $message = $_POST['teamname'];
-         $message .= $_POST['gpocname'];
-         $message .= $_POST['keycloakusername'];
-         $message .= $_POST['adminusers'];
-         
-         $header = "From:abc@somedomain.com \r\n";
-         $header .= "MIME-Version: 1.0\r\n";
-         $header .= "Content-type: text/html\r\n";
+//Import PHPMailer classes into the global namespace
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-         $retval = mail ($to,$subject,$message,$header);
-         
-         if( $retval == true ) {
-            echo "Message sent successfully...";
-         }else {
-            echo "Message could not be sent...";
-         }
-?>
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      
+    $mail->isSMTP();                                            
+    $mail->Host       = 'smtp.example.com';                     
+    $mail->SMTPAuth   = true;                                   
+    $mail->Username   = 'user@example.com';                     
+    $mail->Password   = 'secret';                               
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            
+    $mail->Port       = 465;                                    
+
+    //Recipients
+    $mail->setFrom('from@example.com', 'Mailer');
+    $mail->addAddress('donald.manning@plus3it.com', 'Test User');     
+    $mail->addAddress('ellen@example.com');               
+    $mail->addReplyTo('info@example.com', 'Information');   
+
+    //Content
+    $mail->isHTML(true);                                  
+    $mail->Subject = 'New Service Request';
+    $mail->Body    = $_POST['teamname'];
+    $mail->Body.    = $_POST['gpocname'];
+    $mail->Body.    = $_POST['keycloakusername'];
+    $mail->Body.    = $_POST['adminusers'];
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
