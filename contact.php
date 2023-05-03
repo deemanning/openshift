af656a09b90d57184d39a3c844b74e139898b66b
Dee Manning <donald.manning@plus3it.com>
2023-05-03 10:01:29 -0400

    Fixed syntax within php code

--- contact.php
+++ contact.php
@@ -31,9 +31,9 @@ try {
     $mail->isHTML(true);                                  
     $mail->Subject = 'New Service Request';
     $mail->Body    = $_POST['teamname'];
-    $mail->Body.    = $_POST['gpocname'];
-    $mail->Body.    = $_POST['keycloakusername'];
-    $mail->Body.    = $_POST['adminusers'];
+    $mail.->Body    = $_POST['gpocname'];
+    $mail.->Body    = $_POST['keycloakusername'];
+    $mail.->Body    = $_POST['adminusers'];
 
     $mail->send();
     echo 'Message has been sent';
