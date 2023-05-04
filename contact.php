da8be769a7d8cff6f57fddcd694d34224916c378
Dee Manning <donald.manning@plus3it.com>
2023-05-04 16:13:28 -0400

    Updated php file

--- contact.php
+++ contact.php
@@ -1,10 +1,17 @@
 <?php
-// The message
-$message = "Line 1\r\nLine 2\r\nLine 3";
 
-// In case any of our lines are larger than 70 characters, we should use wordwrap()
-$message = wordwrap($message, 70, "\r\n");
+$to = 'deray2g@yahoo.com';
 
-// Send
-mail('donald.manning@plus3it.com', 'My Subject', $message);
+$subject = 'Hola';
+
+$message = 'This is a test email.';
+
+$retval = mail($to, $subject, $message, [$headers], [$parameters]);
+
+if( $retval == true ) {
+    echo "Message sent successfully...";
+ }else {
+    echo "Message could not be sent...";
+ }
+
 ?>
