c4e4d893af9fb6794675018401a0faabe1d234eb
Dee Manning <donald.manning@plus3it.com>
2023-05-04 16:00:52 -0400

    Test new code changes

--- contact.php
+++ contact.php
@@ -1,32 +1,10 @@
 <?php
-//Import PHPMailer classes into the global namespace
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
+// The message
+$message = "Line 1\r\nLine 2\r\nLine 3";
 
-//This should be done in your php.ini, but this is how to do it if you don't have access to that
-date_default_timezone_set('Etc/UTC');
+// In case any of our lines are larger than 70 characters, we should use wordwrap()
+$message = wordwrap($message, 70, "\r\n");
 
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer();
-
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      
-    $mail->isSMTP();                                            
-    $mail->Host       = 'smtp.example.com';           
-    $mail->Port       = 25;                                    
-
-    //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('donald.manning@plus3it.com', 'Test User');              
-    $mail->addReplyTo('info@example.com', 'Information');   
-
-    //Content                                  
-    $mail->Subject = 'New Service Request';
-    $mail->AltBody    = 'This is the HTML message body <b>in bold!</b>';
-
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
+// Send
+mail('donald.manning@plus3it.com', 'My Subject', $message);
+?>
