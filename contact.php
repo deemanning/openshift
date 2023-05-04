ad98462b6f2508895afd2a511596bea88b051129
Dee Manning <donald.manning@plus3it.com>
2023-05-04 18:14:50 -0400

    Added headers

--- contact.php
+++ contact.php
@@ -6,15 +6,18 @@ $subject = "Hola";
 
 $message = "This is a test email.";
 
-$retval = mail($to, $subject, $message);
+$header = "From: noreply@example.com\r\n";
+$header.= "MIME-Version: 1.0\r\n";
+$header.= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+$header.= "X-Priority: 1\r\n";
 
-if( $retval == true ) {
-    echo "Message sent successfully...";
- }else {
-    echo "Message could not be sent...";
-    error_reporting(-1);
-    ini_set('display_errors', 'On');
-    set_error_handler("var_dump");
- }
+$status = mail($to, $subject, $message, $header);
+
+if($status)
+{
+    echo '<p>Your mail has been sent!</p>';
+} else {
+    echo '<p>Something went wrong. Please try again!</p>';
+}
 
 ?>
