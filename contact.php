b4e7f38f1a966fec15c374986176c083dae9f606
Dee Manning <donald.manning@plus3it.com>
2023-05-05 16:28:00 -0400

    removed header

--- contact.php
+++ contact.php
@@ -6,8 +6,7 @@ $message = $_POST['adminusers'];
 $to = 'deray2g@yahoo.com';
 $from = 'servicesubmission@myserver.com'
 
-$headers  = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
+$headers  = "Content-type: text/html; charset=iso-8859-1" . "\r\n";
 $headers .= "From: ". $from. "\r\n";
 $headers .= "Reply-To: ". $from. "\r\n";
 $headers .= "X-Mailer: PHP/" . phpversion();
