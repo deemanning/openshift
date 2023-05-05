d42d364cf4312bad71f012d019fa992822cd1eec
Dee Manning <donald.manning@plus3it.com>
2023-05-05 16:31:26 -0400

    removed all headers

--- contact.php
+++ contact.php
@@ -6,13 +6,7 @@ $message = $_POST['adminusers'];
 $to = 'deray2g@yahoo.com';
 $from = 'servicesubmission@myserver.com'
 
-$headers  = "Content-type: text/html; charset=iso-8859-1" . "\r\n";
-$headers .= "From: ". $from. "\r\n";
-$headers .= "Reply-To: ". $from. "\r\n";
-$headers .= "X-Mailer: PHP/" . phpversion();
-$headers .= "X-Priority: 1" . "\r\n";
-
-$status = mail($to, $from, $subject, $message, $headers);
+$status = mail($to, $from, $message, $name, $email);
 
 if (isset($_POST['submit'])) 
 {
