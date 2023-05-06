30df7361a5bde19e81034ef8e957794a8984d704
Dee Manning <donald.manning@plus3it.com>
2023-05-06 18:20:44 -0400

    test code

--- contact.php
+++ contact.php
@@ -1,31 +1,9 @@
 <?php
+$to = "deray2g@yahoo.com";
+$subject = "My subject";
+$txt = "Hello world!";
+$headers = "From: webmaster@example.com" . "\r\n" .
+"CC: deray2g@gamil.com";
 
-$message = $_POST['teamname'];
-$message.= $_POST['gpocname'];
-$message.= $_POST['keycloakusername'];
-$message.= $_POST['adminusers'];
-$message.= $_POST['justification'];
-$to = 'deray2g@yahoo.com';
-$from = 'servicesubmission@myserver.com';
-$subject = 'Customer Inquiry';
-$body = "From:" .$from."\r\n E-Mail:" .$subject."\r\n Message:\r\n" .$message;
-
-$headers  = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
-$headers .= "From: ". $from. "\r\n";
-$headers .= "Reply-To: ". $from. "\r\n";
-$headers .= "X-Mailer: PHP/" . phpversion();
-$headers .= "X-Priority: 1" . "\r\n";
-
-$status= mail('deray2g@yahoo.com', $subject, $body, $from, $headers);
-
-if ($_POST['submit']); {
-   if ($status)
-   { 
-       echo '<p>Your message has been sent!</p>';
-   } else { 
-       echo '<p>Something went wrong, go back and try again!</p>'; 
-   }
-}
-
+mail($to,$subject,$txt,$headers);
 ?>
