e65faf275f1dbb3de420a14a55867425009b7034
Dee Manning <donald.manning@plus3it.com>
2023-05-08 08:48:43 -0400

    updated config file

--- SMTPconfig.php
+++ SMTPconfig.php
@@ -1,6 +1,6 @@
 <?php 
-    $SmtpServer="smtp.gmail.com";
-    $SmtpPort="25"; //default
+    $SmtpServer= 'smtp.gmail.com';
+    $SmtpPort= '25'; //default
     $SmtpUser="test";
     $SmtpPass="Pass@wd11";
 ?>
--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@ include('SMTPmail.php');
 if($_SERVER["REQUEST_METHOD"] == "POST")
 {
     $to = "deray2g@yahoo.com";
-    $from = $_POST['mail@example.com'];
+    $from = "mail@example.com";
     $subject = "Enquiry";
     $body = $_POST['teamname'].'</br>'.$_POST['gpocname'].'</br>'.$_POST['adminusers'].'</br>'.$_POST['keycloakusername'].'</br>'.'<hr />'.$_POST['justification'];
     $SMTPMail = new SMTPClient ($SmtpServer, $SmtpPort, $SmtpUser, $SmtpPass, $from, $to, $subject, $body);
