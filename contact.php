33ee2a4e3b919cb31b5e8a41506351c62ee75d50
Dee Manning <donald.manning@plus3it.com>
2023-05-08 10:49:01 -0400

    Udpated contact php file

--- contact.php
+++ contact.php
@@ -1,13 +1,33 @@
-<?php 
+<?php
+
 include('SMTPconfig.php');
-include('SMTPmail.php');
-if($_SERVER["REQUEST_METHOD"] == "POST")
-{
-    $to = "deray2g@yahoo.com";
-    $from = "deray2manning@gmail.com";
-    $subject = "Enquiry";
-    $body = $_POST['teamname'].'</br>'.$_POST['gpocname'].'</br>'.$_POST['adminusers'].'</br>'.$_POST['keycloakusername'].'</br>'.'<hr />'.$_POST['justification'];
-    $SMTPMail = new SMTPClient ($SmtpServer, $SmtpPort, $SmtpUser, $SmtpPass, $from, $to, $subject, $body);
-    $SMTPChat = $SMTPMail->SendMail();
+
+$message = $_POST['teamname'];
+$message.= $_POST['gpocname'];
+$message.= $_POST['keycloakusername'];
+$message.= $_POST['adminusers'];
+$message.= $_POST['justification'];
+$to = 'deray2g@yahoo.com';
+$from = 'servicesubmission@myserver.com';
+$subject = 'Customer Inquiry';
+$body = "From:" .$from."\r\n E-Mail:" .$subject."\r\n Message:\r\n" .$message;
+
+$headers  = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
+$headers .= "From: ". $from. "\r\n";
+$headers .= "Reply-To: ". $from. "\r\n";
+$headers .= "X-Mailer: PHP/" . phpversion();
+$headers .= "X-Priority: 1" . "\r\n";
+
+$status= mail('deray2g@yahoo.com', $subject, $body, $from, $headers);
+
+if ($_POST['submit']); {
+   if ($status)
+   { 
+       echo '<p>Your message has been sent!</p>';
+   } else { 
+       echo '<p>Something went wrong, go back and try again!</p>'; 
+   }
 }
+
 ?>
