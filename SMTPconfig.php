442cf59941ed32168ff873fb4db39a4aa533931f
Dee Manning <donald.manning@plus3it.com>
2023-05-08 08:28:57 -0400

    Continued SMTP testing

---
+++ SMTPconfig.php
@@ -0,0 +1,6 @@
+<?php 
+    $SmtpServer="localhost";
+    $SmtpPort="2525"; //default
+    $SmtpUser="test";
+    $SmtpPass="Pass@wd11";
+?>
--- contact.php
+++ contact.php
@@ -1,35 +1,13 @@
-<?/p
-function send_mail($email='deray2manning@gmail.com', $recipient_name='deray2g@yahoo.com', $message='Testing')
+<?php 
+include('SMTPconfig.php');
+include('SMTPmail.php');
+if($_SERVER["REQUEST_METHOD"] == "POST")
 {
-    require("phpmailer/class.phpmailer.php");
-
-    $mail = new PHPMailer();
-
-    $mail->CharSet = "utf-8";
-    $mail->IsSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = "localhost";  // Specify main and backup server
-    $mail->SMTPAuth = true;     // Turn on SMTP authentication
-    $mail->Username = "deray2manning";  // SMTP username
-    $mail->Password = "Googs12345"; // SMTP password
-
-    $mail->From = "deray2manning@gmail.com";
-    $mail->FromName = "System-Ad";
-    $mail->AddAddress($email, $recipient_name);
-
-    $mail->WordWrap = 50;                                 // Set word wrap to 50 characters
-    $mail->IsHTML(true);                                  // Set email format to HTML (true) or plain text (false)
-
-    $mail->Subject = "This is a Sampleenter code here Email";
-    $mail->Body    = $message;
-    $mail->AltBody = "This is the body in plain text for non-HTML mail clients";
-
-    if(!$mail->Send())
-    {
-       echo "Message could not be sent. <p>";
-       echo "Mailer Error: " . $mail->ErrorInfo;
-       exit;
-    }
-
-    echo "Message has been sent";
+    $to = "deray2g@yahoo.com";
+    $from = $_POST['email'];
+    $subject = "Enquiry";
+    $body = $_POST['name'].'</br>'.$_POST['companyName'].'</br>'.$_POST['tel'].'</br>'.'<hr />'.$_POST['message'];
+    $SMTPMail = new SMTPClient ($SmtpServer, $SmtpPort, $SmtpUser, $SmtpPass, $from, $to, $subject, $body);
+    $SMTPChat = $SMTPMail->SendMail();
 }
 ?>
